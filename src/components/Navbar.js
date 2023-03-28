96e56b7b60807c7c04963c59e0179b80fa3d4943
Carlos Exojo <carlos.exojo-gavilan.external@capgemini.com>
2023-03-28 11:51:15 +0200

    First commit in the dev branch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,9 @@
 function Navbar() {
-  return <div>Hola</div>;
+  return (
+    <div>
+      Hola soy una barra de navegación, para el primer commit de la rama{" "}
+    </div>
+  );
 }
 
 export default Navbar;
